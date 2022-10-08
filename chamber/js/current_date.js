f3ca7391f639fa3c0a70894d6ee691b67b12998f
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2022-10-08 17:00:52 -0600

    Updated

---
+++ chamber/js/current_date.js
@@ -0,0 +1,24 @@
+function toggleMenu(){
+    
+    document.getElementById("primaryNav").classList.toggle("open"); /*this toggles, adding/removing, a class labeled 'open' to our element*/
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+
+
+const today = new Date();
+const longDay = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(today);
+const headerDate = document.querySelector(".headerDate");
+headerDate.innerHTML = `<em>${longDay}</em>`;
+
+
+let modifyDate = new Date(document.lastModified).toLocaleString();
+const dateModified = document.querySelector('#last-update');
+dateModified.textContent = `Last Updated: ${modifyDate}`;
+
+
+const copyright = document.querySelector('#copyright-year');
+let currentYear = today.getFullYear();
+copyright.textContent=`${currentYear}`;
+
