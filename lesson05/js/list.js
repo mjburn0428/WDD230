22f029e61e963a22dac811f804cded67a469f199
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2022-10-12 10:31:28 -0600

    BOM Fav list updated

--- lesson05/js/list.js
+++ lesson05/js/list.js
@@ -1,27 +1,28 @@
+const button = document.querySelector("button");
+const item = document.querySelector("input");
+const list = document.querySelector("ul");
 
-      const list = document.querySelector("ul");
-      const item = document.querySelector("input");
-      const button = document.querySelector("button");
-
-      button.addEventListener("click", () => {
-        const verse = item.value;
-        item.value = "";
-
-        const listItem = document.createElement("li");
-        const listText = document.createElement("span");
-        const listBtn = document.createElement("button");
-
-        listItem.appendChild(listText);
-        listText.textContent = verse;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = "";
-        listBtn.setAttribute("class","delete");
-        list.appendChild(listItem);
-
-        listBtn.addEventListener("click", () => {
-          list.removeChild(listItem);
-        });
-
-        item.focus();
-      });
+button.addEventListener("click", () =>{
+    const chapter = item.value;
+    item.value = "";
+    if (chapter.trim().length !==0){
+       
     
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = chapter;
+    listItem.appendChild(listBtn);
+    listBtn.textContent="X";
+    listBtn.setAttribute("class", "delete");
+    list.appendChild(listItem);
+
+    listBtn.addEventListener("click", () =>{
+        list.removeChild(listItem);
+    });
+    }
+    
+    item.focus();
+})
