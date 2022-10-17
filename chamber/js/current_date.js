1c65ee45f69cb3c5ebe927fc5fe011a73be8e710
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2022-10-17 07:48:32 -0600

    Updated styling and centered text

--- chamber/js/current_date.js
+++ chamber/js/current_date.js
@@ -22,3 +22,12 @@ const copyright = document.querySelector('#copyright-year');
 let currentYear = today.getFullYear();
 copyright.textContent=`${currentYear}`;
 
+function toggleBanner(){
+    document.getElementById("banner").classList.toggle("show");
+}
+const dow = today.getDay();
+if (dow == 1 || dow == 2) {
+    const banner = document.getElementById("banner");
+    banner.textContent = `Come join us for the chamber meet and greet Wednesday at 7:00 pm.`;
+    toggleBanner();
+}
