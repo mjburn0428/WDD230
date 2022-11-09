fdcd5b803afe87fc4f97343b8f61396149fb01ad
mjburn0428 <113367122+mjburn0428@users.noreply.github.com>
2022-11-08 20:40:05 -0700

    Update prophets.js

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://mjburn0428.github.io/WDD230/lesson09/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
 
